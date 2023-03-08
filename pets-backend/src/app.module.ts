1f2cafc9bdfa0b03fe64eb9e1e0434b9c2482b3b
Kenan Omic <kenanomic@Kenans-MacBook-Pro.local>
2023-03-08 16:43:20 +0100

    added pets module

--- pets-backend/src/app.module.ts
+++ pets-backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PetsModule } from './modules/pets/pets.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB)],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB),
+    PetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
